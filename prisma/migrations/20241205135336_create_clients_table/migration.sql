e47468d14e0e36b9dc40e5184905e7eab842fad0
flexgrupo <tecnologia@grupoflex.com.br>
2024-12-05 10:54:20 -0300

    feat: create Client model on prisma schema file

---
+++ prisma/migrations/20241205135336_create_clients_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "birth_date" TIMESTAMP(3) NOT NULL,
+    "subscribed" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Client_email_key" ON "Client"("email");
